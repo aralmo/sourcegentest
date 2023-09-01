25f1d82c0b851fd0b40a034c657560a5938c04cb
aralmo <aralmo@gmail.com>
2023-09-01 12:24:26 +0200

    test sourcegen

---
+++ SourceGen.Demo/Generated.cs
@@ -0,0 +1,18 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceGen.Demo
+{
+    [HelloWorlded]
+    public partial class Generated
+    {
+
+    }
+
+
+
+}
